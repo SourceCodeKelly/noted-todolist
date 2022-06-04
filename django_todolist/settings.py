eaecab8fcc282ad901f9b3ebbfa464808f819015
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-06-04 16:42:34 -0400

    Styling complete; Everything functional

--- django_todolist/settings.py
+++ django_todolist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -128,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main_app/static')]
